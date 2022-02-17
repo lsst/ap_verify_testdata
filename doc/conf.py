7d9d838d149b67991084e423331fb96dae078dcd
Krzysztof Findeisen <kfindeis@uw.edu>
2022-02-16 20:01:30 -0600

    Modernize Sphinx config.

--- doc/conf.py
+++ doc/conf.py
@@ -3,10 +3,11 @@
 This configuration only affects single-package Sphinx documenation builds.
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
+from documenteer.conf.pipelinespkg import *  # noqa F403, import *
 
 
-_g = globals()
-_g.update(build_package_configs(
-    project_name='ap_verify_testdata',))
-
+project = "ap_verify_testdata"
+html_theme_options["logotext"] = project  # noqa F405, unknown name
+html_title = project
+html_short_title = project
+doxylink = {}
